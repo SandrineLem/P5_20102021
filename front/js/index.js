9ed6a54858314182ca0185517072d23abc870522
SandrineLem <lemossandrine@gmail.com>
2021-10-29 12:34:14 +0200

    test Java pour ajouter sur page accueil les produits

--- front/js/index.js
+++ front/js/index.js
@@ -1,3 +1,5 @@
+
+
 var xhr = new XMLHttpRequest();
 xhr.open('GET',"http://localhost:3000/api/products");
 xhr.responseType = 'json';
@@ -11,6 +13,18 @@ xhr.onerror = function () {
 };
 
 xhr.send();
+var canapeProduct = xhr;
+console.log(canapeProduct);
+
+let aProduct = 
+document.getElementById("items").innerHTML = 
+` <a href="..."><article><img src="http://localhost:3000/images/kanap01.jpeg" alt="Photo d'un canapé bleu, deux places">
+<h3 class="productName">Kanap Sinopé</h3>
+<p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p></article></a>`;
+console.log("aProduct");
+
+
+
 
 
 
