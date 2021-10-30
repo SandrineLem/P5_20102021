5859024970040bd150268003647cb5c25d834b12
SandrineLem <lemossandrine@gmail.com>
2021-10-30 16:33:26 +0200

    Ajout du contenu js avec demande api , fonction recup donnees , fonction boucle , modif dom et ajout des donnees dans le don

--- front/js/index.js
+++ front/js/index.js
@@ -1,75 +1,37 @@
-
-//Tableau des données des produits
-var xhr = new XMLHttpRequest();
-xhr.open('GET',"http://localhost:3000/api/products");
-xhr.responseType = 'json';
-
-xhr.onload = function () {
-    console.log(xhr.response);
-};
-
-xhr.onerror = function () {
-    console.log('Booo');
-};
-
-xhr.send();
-//Creation d'une variable Canap
-var canapeData = xhr;
-console.log(canapeData);
-
-
-let infoCanap ={
-
-}
-console.log(document.querySelector)
-//varaible type du produit 1 
-let accueilCanap = 
-document.getElementById("items").innerHTML = 
-` <a href="product.html"><article><img src="http://localhost:3000/images/kanap01.jpeg" alt="Photo d'un canapé bleu, deux places">
-<h3 class="productName">Kanap Sinopé</h3>
-<p class="productDescription">Dis enim malesuada risus sapien gravida
- nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p></article></a>
-
-  <a href="product.html"><article><img src="http://localhost:3000/images/kanap02.jpeg" alt="Photo d'un canapé jaune et noir, quattre places">
-<h3 class="productName">Kanap Cyllène</h3>
-<p class="productDescription">Morbi nec erat aliquam, sagittis urna non, laoreet justo.
- Etiam sit amet interdum diam, at accumsan lectus.</p></article></a>
-
- <a href="product.html"><article><img src="http://localhost:3000/images/kanap03.jpeg" alt="Photo d'un canapé d'angle, vert, trois places">
-<h3 class="productName">Kanap Calycé</h3>
-<p class="productDescription">Pellentesque fermentum arcu venenatis ex sagittis accumsan.
- Vivamus lacinia fermentum tortor.Mauris imperdiet tellus ante..
- Etiam sit amet interdum diam, at accumsan lectus.</p></article></a>
  
- <a href="product.html"><article><img src="http://localhost:3000/images/kanap04.jpeg" alt="Photo d'un canapé rose, une à deux place">
- <h3 class="productName">Kanap Autonoé</h3>
- <p class="productDescription">Donec mattis nisl tortor, nec blandit sapien fermentum at.
-  Proin hendrerit efficitur fringilla. Lorem ipsum dolor sit amet.</p></article></a>
-  
-  <a href="product.html"><article><img src="http://localhost:3000/images/kanap05.jpeg" alt="Photo d'un canapé gris, trois places">
- <h3 class="productName">Kanap Eurydomé</h3>
- <p class="productDescription">Ut laoreet vulputate neque in commodo. Suspendisse maximus quis erat in sagittis.
-  Donec hendrerit purus at congue aliquam.</p></article></a>
-  
-  <a href="product.html"><article><img src="http://localhost:3000/images/kanap06.jpeg" alt="Photo d'un canapé gris, deux places">
- <h3 class="productName">Kanap Hélicé</h3>
- <p class="productDescription">Curabitur vel augue sit amet arcu aliquet interdum. Integer vel quam mi.
-  Morbi nec vehicula mi, sit amet vestibulum.</p></article></a>
-  
-  <a href="product.html"><article><img src="http://localhost:3000/images/kanap07.jpeg" alt="Photo d'un canapé rouge, deux places">
-  <h3 class="productName">Kanap Thyoné</h3>
-  <p class="productDescription">EMauris imperdiet tellus ante, sit amet pretium turpis molestie eu.
-   Vestibulum et egestas eros. Vestibulum non lacus orci.</p></article></a>
-   
-   <a href="product.html"><article><img src="http://localhost:3000/images/kanap08.jpeg" alt="Photo d'un canapé rose, trois places">
-   <h3 class="productName">Kanap orthosie</h3>
-   <p class="productDescription">Mauris molestie laoreet finibus. Aenean scelerisque convallis lacus at dapibus.
-    Morbi imperdiet enim metus rhoncus.</p></article></a>`;
-console.log(typeof accueilCanap);
-console.log(accueilCanap);
+//Fonction global "sans nom" qui fera tout le temps la meme chose
+//Fonction pour recuperer les donnees et les mettre dans le dom page accueil des le chargement de la page  
+// Fonction "getArticles pour 1 - Recup des articles 
+(async function () {// ajout du "async pour que fonction "await"
+    const articles = await getArticles() // ajout du await pour attendre les donnees pour le return fetch 
+    
+    for (article of articles) { // boucle article / article (du total des articles) 
+        displayArticle(article) // afficher tout les article(s)
+        console.log(articles)
+    }
+})()// on appelle la fonction grace au parentaises
 
-
-
-
-
+//Fonction pour recuperer les donnees de l API des articles 
+function getArticles(){
+    return fetch("http://localhost:3000/api/products")
+    .then (function(httpBodyResponse){ // fonction quand il recupere les donnees en httpBody
+        return httpBodyResponse.json() // transfromation de httpBody  en json 
+    })
+    .then(function(articles){ // recuperer le json renommé en "articles"
+        return articles        // reponse return le contenu json "articles"
+    })
+    .catch(function(error){    // si erreur fonction d'afficher une alert 'error' 
+        alert(error)
+    })
+}
+// utiliser notre fonction afficher les articles avec les donnees article (Json), Modification du dom avec innerHtml avec id item 
+function displayArticle(article){
+    document.getElementById("items") .innerHTML += `<a href="product.html?id=${article._id}">
+    <article>
+      <img src="${article.imageUrl}" alt="${article.altTxt}">
+      <h3 class="productName">${article.name}</h3>
+      <p class="productDescription">${article.description}</p>
+    </article></a>`
+}
+// "${}" inclure les donnees du fichier article (json)
 
